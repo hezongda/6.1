af2111d32f82f927ebc13b4c45a7f1e02f923935
hezongda <38730027+hezongda@users.noreply.github.com>
2018-05-09 12:27:56 +0800

    Update 6th.py

--- 6th.py
+++ 6th.py
@@ -1,38 +1,47 @@
 import numpy as np
 
 k=0 #记录循环次数
-tr=0    #初始判断值
+tr=0    #用于循环二分法
+t=0     #用于当解不在范围内时循环输入
 
 func=input('请输入函数：')
 def f(x):
     return eval(func)
 
-a=float(input('请输入a:'))
-b=float(input('请输入b:'))
-er=float(input('请输入精确度er:'))
+while t==0:
+    a=float(input('请输入a:'))
+    b=float(input('请输入b:'))
+    er=float(input('请输入精确度er:'))
 
-while tr==0:
-    if f(a)*f(b)>0:
-        print('解不在范围内，请重试')
-        break
+    while tr==0:
+        if f(a)*f(b)>0:
+            print('解不在范围内，请重试')
+            print( )
+            break
 
-    elif f(a)*f(b)==0:
-        if f(a)==0:
-            print(a,k)
-            tr=1
-        else:
-            print(b,k)
-            tr = 1
-
-    else:
-        m=0.5*(a+b)
-
-        if abs(b-a)<er:
-            print(m,k)
-            tr = 1
-        else:
-            if f(a)*f(m)>0:
-                a=m
+        elif f(a)*f(b)==0:
+            if f(a)==0:
+                print('方程的解是：',a)
+                print('循环次数：',k)
+                tr=1
+                t=1
             else:
-                b=m
-            k=k+1
+                print('方程的解是：',b)
+                print('循环次数：', k)
+                tr = 1
+                t=1
+
+        else:
+            m=0.5*(a+b)
+
+            if np.abs(b-a)<er:
+                print('方程的解是：',m)
+                print('循环次数：', k)
+                tr = 1
+                t=1
+            else:
+                if f(a)*f(m)>0:
+                    a=m
+                else:
+                    b=m
+                k=k+1
